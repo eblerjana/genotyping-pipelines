ec638a4a9a7a4484a69ceab2b16b49a2824ea15e
Jana Ebler <ebler@hhu.de>
2023-10-13 16:53:52 +0200

    continued working on merging script, still not functional

--- prepare-rare-variant-panel/workflow/scripts/combine_vcfs.py
+++ prepare-rare-variant-panel/workflow/scripts/combine_vcfs.py
@@ -11,7 +11,7 @@ class VcfRecord:
 	"""
 	Represents a VCF record.
 	"""
-	def __init__(self, line, ):
+	def __init__(self, line, sample_names):
 		fields = line.strip().split()
 		self._chrom = fields[0]
 		self._pos = int(fields[1])
@@ -23,7 +23,12 @@ class VcfRecord:
 		self._filter = fields[6]
 		self._info = fields[7]
 		self._format = fields[8]
-		self._samples = fields[9:]
+		if len(sample_names) != len(fields[9:]):
+			raise RuntimeError("VcfRecord: number of sample names does not match the number of sample columns in the VCF line.")
+		self._sample_to_field = {}
+		for name, data in zip(sample_names, fields[9:]):
+			self._sample_to_field[name] = data
+
 
 	def __eq__(self, other):
 		if self._chrom != other._chrom:
@@ -44,9 +49,15 @@ class VcfRecord:
 			return False
 		if self._format != other._format:
 			return False
-		if self._samples != other._samples:
+		if self._sample_to_field != other._sample_to_field:
 			return False
 		return True
+
+	def __lt__(self, other):
+		if self._chrom != other._chrom:
+			return self._chrom < other._chrom
+		else:
+			return self._pos < other._pos
 
 	def chrom(self):
 		return self._chrom
@@ -67,7 +78,7 @@ class VcfRecord:
 		"""
 		Return true, if the variant is representing the same
 		event as variant "other". This is the case if the
-		position and ref allele are identical.
+		position, ref allele and alt alleles are identical.
 		"""
 
 		if self._chrom != other._chrom:
@@ -79,6 +90,10 @@ class VcfRecord:
 		if self._ref != other._ref:
 			return False
 		
+		alt_alleles = set(self._alt)
+		other_alt_alleles = set(other._alt)
+		if alt_alleles != other_alt_alleles:
+			return False
 		return True
 
 	def combine_variants(self, other):
@@ -86,6 +101,10 @@ class VcfRecord:
 		Combine two variants into one record. The variants
 		need to match in terms of position and reference allele.
 		"""
+		
+		# make sure that the records do not have overlapping samples
+		if bool(set([s for s in self._sample_to_field]) & set([s for s in other._sample_to_field])):
+			raise RuntimeError('VcfRecord: combine_variants: variants can only be combined if the sample columns are disjoint.')
 
 		if not self.same_variant_location(other):
 			raise RuntimeError('VcfRecord: combine_variants: variants can only be combined if they represent the same allele.')
@@ -101,8 +120,8 @@ class VcfRecord:
 		# add sample columns and update genotypes
 		# index of GT field within samples
 		gt_index = other._format.split(':').index('GT')
-		for i in range(len(other._samples)):
-			genotype = other._samples[i].split(':')[gt_index]
+		for name, data in other._sample_to_field.items():
+			genotype = data.split(':')[gt_index]
 			updated_genotype = []
 			separator = '|' if '|' in genotype else '/'
 
@@ -111,27 +130,59 @@ class VcfRecord:
 					updated_genotype.append(str(new_indices[int(a)]))
 				else:
 					updated_genotype.append(a)
-			updated_sample_field = other._samples[i].split(':')
+			updated_sample_field = data.split(':')
 			updated_sample_field[gt_index] = separator.join(updated_genotype)
-			self._samples.append(':'.join(updated_sample_field))
-	
-	def add_samples(self, other_samples):
-		"""
-		Add sample information as additional columns to
-		the VCF record.
-		"""
-		self._samples += other_samples
-	
-	def __str__(self):
-		outline = '\t'.join([self._chrom, str(self._pos), self._id, self._ref, ','.join(self._alt), self._qual, self._filter, self._info, self._format, '\t'.join(self._samples)])
-		return outline
-		
-		
-# TODO: in merging, how to know which samples need to be set to 0/0 for missing variants?
-# maybe keep track in variant record which genotypes correspond to which samples / indices in final VCF sample columns?
+			self._sample_to_field[name] = ':'.join(updated_sample_field)
+
+
+	def str_record(self, sample_names = [], is_phased = False):
+		sample_columns = list(set([s for s in self._sample_to_field.keys()] + sample_names))
+		outline = [self._chrom, str(self._pos), self._id, self._ref, ','.join(self._alt), self._qual, self._filter, self._info, self._format]
+		for sample in sorted(sample_columns):
+			if sample in self._sample_to_field:
+				outline.append(self._sample_to_field[sample])
+			else:
+				outline.append("0|0" if is_phased else "0/0")
+		return '\t'.join(outline)
+
+
+def find_overlaps(variants):
+	"""
+	Finds records sharing position and
+	REF allele. These are the ones that
+	are to be combined into one record later.
+	"""
+	assert len(variants) > 1
+	current_position = variants[0].pos()
+	current_chrom = variants[0].chrom()
+	current_cluster = [variants[0]]
+	for v in variants[1:]:
+		if (v.pos() != current_position) or (v.chrom() != current_chrom) or (v.ref() != current.ref()):
+			yield current_cluster
+			current_cluster = []
+		current_chrom = v.chrom()
+		current_cluster.append(v)
+		current_pos = v.pos()
+	if len(current_cluster) != 0:
+		yield current_cluster
+
+
+def create_combined_cluster(cluster):
+	"""
+	Given records that share locations and
+	REF alleles, combined them into one single
+	record.
+	"""
+	assert len(cluster) > 1
+	combined_record = cluster[0]
+	for c in cluster[1:]:
+		combined_record.combine_variants(c)
+	return c
+
+
 def run_combine_vcfs(vcfs):
 	vcf_to_samples = {}
-	variants = 
+	variants = []
 	for vcf in vcfs:
 		for line in open(vcf, 'r'):
 			if line.startswith('##'):
@@ -141,8 +192,24 @@ def run_combine_vcfs(vcfs):
 				samples = line.strip().split('\t')[9:]
 				vcf_to_samples[vcf] = samples
 				continue
+			assert vcf in vcf_to_samples
+			variant = VcfRecord(line, vcf_to_samples[vcf])
+			variants.append(variant)
+	variants.sort()
+	print(variants)
+	for cluster in find_overlaps(variants):
+		combined_record = create_combined_record(cluster)
+		
 			
-			 
+# TODOs:
+# write header of VCF properly
+# remove information other than the GT field (write header accordingly)
+# when and when not to combine variants??
+# Plan: 
+#	restrict script to biallelic VCFs only --> first check if my merging script is compartible with that
+#	require identical alleles for variants to be the same
+#	sorting also according to alleles, so that identical records are always ascending
+	 
 		
 	
 
---
+++ prepare-rare-variant-panel/workflow/scripts/test_combine_vcfs.py
@@ -0,0 +1,142 @@
+import unittest, importlib
+import tempfile
+import io
+import gzip
+from contextlib import redirect_stdout
+from combine_vcfs import VcfRecord
+
+
+class Test_VcfRecord(unittest.TestCase):
+	def test_vcfrecord(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		record = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		self.assertEqual(record.chrom(), "chr1")
+		self.assertEqual(record.pos(), 1110696)
+		self.assertEqual(record.id(), "rs6040355")
+		self.assertEqual(record.ref(), "A")
+		self.assertEqual(record.alt(), ["G", "T"])
+
+
+	def test_same_variant(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		vcf_line2 = "chr1\t1110696\trs6040355\tA\tC\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+		
+		self.assertTrue(record1.same_variant_location(record2))
+		self.assertTrue(record1.same_variant_location(record1))
+
+		vcf_line3 = "chr1\t1110697\trs6040355\tA\tC\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t0|1:21:6:23,27\t1|1:2:0:18,2\t0/1:35:4"
+		record3 = VcfRecord(vcf_line3, ["sample1", "sample2", "sample3"])
+		self.assertFalse(record3.same_variant_location(record1))
+		self.assertFalse(record3.same_variant_location(record2))
+
+		vcf_line4 = "chr2\t1110697\trs6040355\tA\tC\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t0|1:21:6:23,27\t1|1:2:0:18,2\t0/1:35:4"
+		record4 = VcfRecord(vcf_line4, ["sample1", "sample2", "sample3"])
+		self.assertFalse(record4.same_variant_location(record3))
+		self.assertFalse(record4.same_variant_location(record1))
+		
+	
+	def test_combine_variants1(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		vcf_line2 = "chr1\t1110696\trs6040355\tA\tT\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t0|1:21:6:23,27\t1|1:2:0:18,2\t0/1:35:4"
+		expected = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4\t0|2:21:6:23,27\t2|2:2:0:18,2\t0/2:35:4"
+		
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+		expected_record = VcfRecord(expected, ["sample1", "sample2", "sample3", "sample4", "sample5", "sample6"])
+		self.assertTrue(record1.same_variant_location(record2))
+
+		record1.combine_variants(record2)
+		self.assertTrue(expected_record == record1)
+		self.assertTrue(expected_record.str_record() == record1.str_record())
+
+
+	def test_combine_variants2(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		vcf_line2 = "chr2\t1110696\trs6040355\tA\tT\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t0|1:21:6:23,27\t1|1:2:0:18,2\t0/1:35:4"
+		
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+		self.assertRaises(RuntimeError, record1.combine_variants, record2)
+
+	def test_combine_variants3(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4"
+		vcf_line2 = "chr1\t1110696\trs6040355\tA\tT,G\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t0|1:21:6:23,27\t1|2:2:0:18,2\t0/2:35:4"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+
+		record1.combine_variants(record2)
+		expected = "chr1\t1110696\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB\tGT:GQ:DP:HQ\t1|2:21:6:23,27\t2|1:2:0:18,2\t2/2:35:4\t0|2:21:6:23,27\t2|1:2:0:18,2\t0/1:35:4"
+		expected_record = VcfRecord(expected, ["sample1", "sample2", "sample3", "sample4", "sample5", "sample6"])
+
+		self.assertTrue(expected_record == record1)
+
+	def test_combine_variants4(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2"
+		vcf_line2 = "chr1\t1110696\trs6040355\tATTG\tT,GA,CTG\t67\tPASS\t.\tGT:GQ:DP:HQ\t0|1\t1|2\t0/3"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+
+		record1.combine_variants(record2)
+		expected = "chr1\t1110696\trs6040355\tATTG\tGA,T,CTG\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2\t0|2\t2|1\t0/3"
+		expected_record = VcfRecord(expected, ["sample1", "sample2", "sample3", "sample4", "sample5", "sample6"])
+
+		self.assertTrue(expected_record == record1)
+
+
+	def test_combine_variants5(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2"
+		vcf_line2 = "chr1\t1110696\trs6040355\tATTG\tCTG\t67\tPASS\t.\tGT:GQ:DP:HQ\t0|1\t1|0\t0/0"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample4", "sample5", "sample6"])
+
+		record1.combine_variants(record2)
+		expected = "chr1\t1110696\trs6040355\tATTG\tGA,T,CTG\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2\t0|3\t3|0\t0/0"
+		expected_record = VcfRecord(expected,  ["sample1", "sample2", "sample3", "sample4", "sample5", "sample6"])
+		self.assertTrue(expected_record == record1)
+
+
+	def test_combine_variants6(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		str_rec = record1.str_record()
+		expected = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2"
+		self.assertTrue(str_rec == expected)
+
+		str_rec = record1.str_record(sample_names = ["sample4", "sample5"])
+		expected = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2\t0/0\t0/0"
+		self.assertTrue(str_rec == expected)
+		
+	def test_combine_variants7(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT:GQ:DP:HQ\t1|2\t2|1\t2/2"
+		vcf_line2 = "chr1\t1110696\trs6040355\tATTG\tCTG\t67\tPASS\t.\tGT:GQ:DP:HQ\t0|1\t1|0\t0/0"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample2", "sample5", "sample6"])
+
+		# should raise an error because samples are overlapping
+		self.assertRaises(RuntimeError, record1.combine_variants, record2)
+
+
+# TODO
+def Test_find_overlaps(unittest.TestCase):
+	def test_find_overlaps1(self):
+		vcf_line1 = "chr1\t1110696\trs6040355\tATTG\tGA,T\t67\tPASS\t.\tGT\t1|2\t2|1\t2/2"
+		vcf_line2 = "chr1\t1110696\trs6040355\tATTG\tCTG\t67\tPASS\t.\tGT\t0|1\t1|0\t0/0"
+		vcf_line3 = "chr1\t1110696\trs6040355\tATT\tCTG\t67\tPASS\t.\tGT\t0|1\t1|0\t0/0"
+		record1 = VcfRecord(vcf_line1, ["sample1", "sample2", "sample3"])
+		record2 = VcfRecord(vcf_line2, ["sample1", "sample2", "sample3"])
+		record3 = VcfRecord(vcf_line3, )
+		
+		pass
+
+
+# TODO
+def Test_create_combined_clusters(unittest.TestCase):
+	def test_create_combine(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
